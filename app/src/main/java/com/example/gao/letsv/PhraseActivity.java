5a80defa5235a89dde6f4e19daa66b2462a45faa
DK714 <dk150114@163.com>
2018-04-26 11:31:07 +0800

    phrase

---
+++ app/src/main/java/com/example/gao/letsv/PhraseActivity.java
@@ -0,0 +1,15 @@
+package com.example.gao.letsv;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dk150 on 2018/4/25.
+ */
+
+public class PhraseActivity extends AppCompatActivity {
+    protected void onCreat(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.phrasestudy);
+    }
+}
