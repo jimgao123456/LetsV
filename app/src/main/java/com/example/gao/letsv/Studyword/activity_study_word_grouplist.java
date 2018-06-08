1648aea86b2587b09959dd2512c62fa11057d051
SundayZhao <664834952@qq.com>
2018-06-08 15:41:53 +0800

    Merge branch 'master' of https://github.com/jimgao123456/Letsv
    # Conflicts:
    #	.idea/modules.xml

---
+++ app/src/main/java/com/example/gao/letsv/Studyword/activity_study_word_grouplist.java
@@ -0,0 +1,189 @@
+package com.example.gao.letsv.Studyword;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.example.gao.letsv.MainActivity;
+import com.example.gao.letsv.MyListAdatper.MyAdapter_study_main;
+import com.example.gao.letsv.R;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+import cz.msebera.android.httpclient.Header;
+
+/**
+ * Created by gangchang on 2018/6/8.
+ */
+
+public class activity_study_word_grouplist extends AppCompatActivity implements View.OnTouchListener, GestureDetector.OnGestureListener {
+    private GestureDetector gd = null;
+    private TextView titleview = null;
+    private TextView wordlist = null;
+    private TextView memoryview = null;
+
+    private static String requstrul = "http://58.87.108.125:8888/wordgrouplist";
+    private JSONObject respondsjson = null;
+    private String[] words_array = null;
+
+    static String[] units = {"", "十", "百", "千", "万", "十万", "百万", "千万", "亿",
+            "十亿", "百亿", "千亿", "万亿"};
+    static char[] numArray = {'零', '一', '二', '三', '四', '五', '六', '七', '八', '九'};
+
+    @Override
+    protected void onCreate(Bundle saveinbundle) {
+        super.onCreate(saveinbundle);
+
+        //滑动手势相关
+        gd = new GestureDetector((GestureDetector.OnGestureListener) this);
+
+        titleview = (TextView) findViewById(R.id.study_word_grouplist_grouptitle);
+
+        wordlist = (TextView) findViewById(R.id.study_word_grouplist_groupwords);
+
+        memoryview = (TextView) findViewById(R.id.study_word_grouplist_Memory);
+    }
+
+    private void iniData() {
+        MainActivity.num = Integer.toString(Integer.parseInt(MainActivity.num) + 1);
+        titleview.setText("第" + foematInteger(Integer.parseInt(MainActivity.num)) + "组");
+         /*
+        *url=http://58.87.108.125:8888/wordgrouplist
+        *请求
+        *JSON格式
+        *
+        * 接收
+        * {
+        * "WORDS":"word1 \r\n  wor2 \r\n  word3 \r\n",//换行
+        * "MEMORYMETHOD":"其实我也不知道怎么记忆,balabalabala"，
+         */
+        SweetAlertDialog pDialog = new SweetAlertDialog(activity_study_word_grouplist.this, SweetAlertDialog.PROGRESS_TYPE);
+        pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+        pDialog.setTitleText("Loading");
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        //params.put("word", word);
+        String url = requstrul;
+        client.post(url, params, new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String str = new String(responseBody);
+                respondsjson = JSON.parseObject(str);
+                wordlist.setText(respondsjson.getString("WORDS"));
+                memoryview.setText(respondsjson.getString("MEMORYMETHOD"));
+                words_array = respondsjson.getString("WORDS").split("\r\n");
+                pDialog.cancel();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                pDialog.setTitleText("未知错误")
+                        .changeAlertType(SweetAlertDialog.ERROR_TYPE);
+                pDialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(SweetAlertDialog sweetAlertDialog) {
+                        finish();
+                    }
+                });
+            }
+        });
+
+    }
+
+    private static String foematInteger(int num) {
+        char[] val = String.valueOf(num).toCharArray();
+        int len = val.length;
+        System.out.println("----" + len);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            String m = val[i] + "";
+            int n = Integer.valueOf(m);
+            boolean isZero = n == 0;
+            String unit = units[(len - 1) - i];
+            if (isZero) {
+                if ('0' == val[i - 1]) {
+                    //当前val[i]的下一个值val[i-1]为0则不输出零
+                    continue;
+                } else {
+                    //只有当当前val[i]的下一个值val[i-1]不为0才输出零
+                    sb.append(numArray[n]);
+                }
+            } else {
+                sb.append(numArray[n]);
+                sb.append(unit);
+            }
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    //e1  The first down motion event that started the fling.手势起点的移动事件
+//e2  The move motion event that triggered the current onFling.当前手势点的移动事件
+//velocityX   The velocity of this fling measured in pixels per second along the x axis.每秒x轴方向移动的像素
+//velocityY   The velocity of this fling measured in pixels per second along the y axis.每秒y轴方向移动的像素
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+
+        if (e1.getX() - e2.getX() > 100 && Math.abs(velocityX) > 50) {
+            //向左滑,开始学习
+            if (words_array != null) {
+                Intent intent = new Intent(this, activity_study_word_main.class);
+                Bundle bundle = new Bundle();
+                bundle.putStringArray("words", words_array);
+                startActivity(intent);
+                finish();
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return gd.onTouchEvent(motionEvent);
+    }
+}
---
+++ app/src/main/java/com/example/gao/letsv/Studyword/activity_study_word_test.java
@@ -0,0 +1,198 @@
+package com.example.gao.letsv.Studyword;
+
+import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.example.gao.letsv.MyListAdatper.MyAdapter_study_test;
+import com.example.gao.letsv.MyWidget.UnScrollListView;
+import com.example.gao.letsv.R;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+import cz.msebera.android.httpclient.Header;
+
+/**
+ * Created by gangchang on 2018/6/5.
+ */
+
+public class activity_study_word_test extends AppCompatActivity{
+    private TextView wordtitle = null;
+    private ImageView wordhorn = null;
+    private UnScrollListView word_selectmeans_listview = null;
+    private ImageView unkonowword = null;
+    private ImageView knowword = null;
+
+    private List<Map<String, Object>> dataList = null;
+    private JSONObject respondsjson = null;
+    private String[] words_array = null;
+    private String cur_word = null;
+    private int cur_word_sit = 0;
+    private String musicurl = null;
+
+    private static String requstrul = "http://58.87.108.125:8888/testword";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //获取本组单词词表
+        setContentView(R.layout.study_word);
+        Bundle bundle = getIntent().getExtras();
+        words_array = bundle.getStringArray("words");
+        cur_word_sit = 0;
+        cur_word = words_array[cur_word_sit];
+
+        //单词标题
+        wordtitle = (TextView) findViewById(R.id.study_word_test_title);
+
+        //喇叭
+        wordhorn = (ImageView) findViewById(R.id.study_word_test_horn);
+
+        //待选列表
+        word_selectmeans_listview = (UnScrollListView) findViewById(R.id.study_word_test_listitem);
+
+        //不认识按钮
+        unkonowword = (ImageView) findViewById(R.id.study_word_test_button_unknow);
+
+        //认识按钮
+        knowword = (ImageView) findViewById(R.id.study_word_test_button_know);
+
+        //刷新界面
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                initDataList(cur_word);
+            }
+        });
+
+    }
+
+
+    private void initDataList(String word) {
+        /*
+        *url=http://58.87.108.125:8888/testword
+        *请求
+        *JSON格式
+        * word="insistence"
+        * 接收
+        * {
+        * "WORD":"persist,//不用html修饰
+        * "MUISC":"http://www.baidu.com",
+        * "OTHER":"{
+        *           "0":"n.强调",
+        *           "1":"v.强势",
+        *           "2":"n.坚持",
+        *           "3":"n.实例",
+        *           ....
+        *           "right":"2"
+        *           }
+        * }
+         */
+        SweetAlertDialog pDialog = new SweetAlertDialog(activity_study_word_test.this, SweetAlertDialog.PROGRESS_TYPE);
+        pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+        pDialog.setTitleText("Loading");
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("word", word);
+        String url = requstrul;
+        client.post(url, params, new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String str = new String(responseBody);
+                respondsjson = JSON.parseObject(str);
+                wordtitle.setText(Html.fromHtml(respondsjson.getString("WORD")));
+                musicurl = respondsjson.getString("MUSIC");
+                wordhorn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //播放音乐
+                        MediaPlayer mediaPlayer = new MediaPlayer();
+                        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);// 设置媒体流类型
+                        try {
+                            mediaPlayer.setDataSource(musicurl);
+                            mediaPlayer.prepare();
+                            mediaPlayer.start();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+
+                JSONObject jsonohter = JSON.parseObject(respondsjson.getString("OTHER"));
+                int step = 0;
+                dataList = new ArrayList<Map<String, Object>>();
+                while (true) {
+                    String title = null;
+                    if ((title = jsonohter.getString(Integer.toString(step))) == null) {
+                        break;
+                    }
+                    Map<String, Object> map = new HashMap<String, Object>();
+                    map.put("selecttext", title);
+                    map.put("right", (Integer.parseInt(jsonohter.getString("right")) == step) ? "0" : "1");
+                    dataList.add(map);
+                    step++;
+                }
+                MyAdapter_study_test adapter = new MyAdapter_study_test(getApplicationContext(), dataList, R.layout.study_word_test_listitem);
+                word_selectmeans_listview.setAdapter(adapter);
+                word_selectmeans_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        String t = adapterView.getItemAtPosition(i).toString();
+                        if(t.charAt(0)=='0'){
+                            //正确
+                        }else{
+                            //错误
+                        }
+                        if(cur_word_sit<words_array.length-1) {
+                            cur_word_sit++;
+                            cur_word=words_array[cur_word_sit];
+                            initDataList(cur_word);
+                        }else{
+                            //开启下一组
+                            finish();
+                        }
+                    }
+                });
+                pDialog.cancel();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                pDialog.setTitleText("未知错误")
+                        .changeAlertType(SweetAlertDialog.ERROR_TYPE);
+                pDialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(SweetAlertDialog sweetAlertDialog) {
+                        finish();
+                    }
+                });
+            }
+        });
+    }
+
+    public void select_correct() {
+
+    }
+}
