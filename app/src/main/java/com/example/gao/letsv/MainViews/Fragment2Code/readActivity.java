1cf5a24f148e6febd429c80afd386111df532a8c
astrophelX <1564920673@qq.com>
2018-06-15 00:16:47 +0800

    新的需求已经日狗

---
+++ app/src/main/java/com/example/gao/letsv/MainViews/Fragment2Code/readActivity.java
@@ -0,0 +1,54 @@
+package com.example.gao.letsv.MainViews.Fragment2Code;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.gao.letsv.R;
+
+public class readActivity extends AppCompatActivity {
+
+    private Button mBtnRecordAudio;
+    private Button mBtnPlayAudio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read);
+        initView();
+        mBtnRecordAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final RecordAudioDialogFragment fragment = RecordAudioDialogFragment.newInstance();
+                fragment.show(getSupportFragmentManager(), RecordAudioDialogFragment.class.getSimpleName());
+                fragment.setOnCancelListener(new RecordAudioDialogFragment.OnAudioCancelListener() {
+                    @Override
+                    public void onCancel() {
+                        fragment.dismiss();
+                    }
+                });
+            }
+        });
+
+        mBtnPlayAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RecordingItem recordingItem = new RecordingItem();
+                SharedPreferences sharePreferences = getSharedPreferences("sp_name_audio", MODE_PRIVATE);
+                final String filePath = sharePreferences.getString("audio_path", "");
+                long elpased = sharePreferences.getLong("elpased", 0);
+                recordingItem.setFilePath(filePath);
+                recordingItem.setLength((int) elpased);
+                PlaybackDialogFragment fragmentPlay = PlaybackDialogFragment.newInstance(recordingItem);
+                fragmentPlay.show(getSupportFragmentManager(), PlaybackDialogFragment.class.getSimpleName());
+            }
+        });
+    }
+
+    private void initView() {
+        mBtnRecordAudio = (Button)findViewById(R.id.main_btn_record_sound);
+        mBtnPlayAudio = (Button) findViewById(R.id.main_btn_play_sound);
+    }
+}
